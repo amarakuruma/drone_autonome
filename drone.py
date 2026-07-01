eb95cc8c271fac0c1078ca44d744d220d08d034b
amarakuruma <kouroumaamara99@gmail.com>
2026-07-01 23:19:25 +0300

    naviagation autonome par point

--- drone.py
+++ drone.py
@@ -3,8 +3,14 @@ nom = "drone 001"
 vitesse = 0 
 altitude = 0 
 batterie = 100
-position = {"x": 0, "y": 0, "z": 0}
+position ={"x":0,"y":0,"z":0}
 en_vol = False # type: ignore
+trajet =[
+    {"x": 10, "y": 20, "z": 30},
+    {"x": 15, "y": 25, "z": 35},
+    {"x": 20, "y": 30, "z": 40},
+    {"x": 25, "y": 35, "z": 45}
+]
 
 print("initialisation du drone")
 print("nom :", nom)
@@ -24,8 +30,6 @@ def decoller():
         print("altitude actuelle:", altitude)
     else:
         print("Le drone est déjà en vol.")
-
-decoller()
 def atterrir():
     global en_vol
     global altitude
@@ -36,21 +40,23 @@ def atterrir():
         print(" altitude actuelle:", altitude)
     else:
         print("Le drone est deja au sol")
-atterrir()
-
 def voler():
     global en_vol
     global batterie
     decoller()
     en_vol = True
     print("Le drone commence à voler.")
-    while en_vol:
-        print("Le drone est en vol.")
-        print("altitude actuelle:", altitude)
-        print("position actuelle:", position)
-        print("batterie restante:", batterie)
-        batterie -=10
+    for point in trajet:
+        position["x"] = point["x"]
+        position["y"] = point["y"]
+        position["z"] = point["z"]
+        print("Le drone se déplace vers la position:", position)
+        batterie -= 10
+    
         if batterie <= 0:
             print("Batterie faible. Le drone doit atterrir.")
             break
+    atterrir()
+
 voler()
+
